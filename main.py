0328d8e9102310d32188d568083de34a72cff39a
Artyr <boltinovartua@gmail.com>
2024-12-16 15:49:38 +0300

    homework

---
+++ main.py
@@ -0,0 +1,10 @@
+def test_function():
+
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+    return
+test_function()
+# inner_function() не работает так как функция находится вне области видимоитси
